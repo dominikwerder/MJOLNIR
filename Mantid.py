54f367f56b227ad511bf5fb0ab0d088b940c4d65
Jakob Lass <lass.jakob@gmail.com>
2018-03-20 22:37:39 +0100

    Add Mantid.py

---
+++ Mantid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 22:27:24 2018
+
+@author: lass
+"""
+
+import subprocess
+
+
+mantidPath = str(subprocess.check_output("dpkg -L Mantid |grep mantidpython", shell=True)[:-1])[2:-1]
+
+print(subprocess.check_output(mantidPath+' mantidtest.py',shell=True))
